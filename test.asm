ca1a819e0c13104fc694a98247d5b67669768e82
wentao he <2011128602@qq.com>
2022-10-25 22:00:24 +0800

    linux commit

--- test.asm
+++ test.asm
@@ -5,7 +5,6 @@ SECTION .data                     ; Section for variable definitions
 
 ; Variable containing a string that has a line break and is null-terminated
 string:   DB "Hello\n",0ah
-
 ; Variable that calculates the value of an expression to determine the
 ; length, in bytes, of the variable "stringLiteral" by subtracting the
 ; starting memory address of the variable from the current memory address
